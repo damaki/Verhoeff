548f823feba169978333d86fbcc5726161be995a
Daniel King <damaki.gh@gmail.com>
2016-02-10 14:40:35 -0400

    Change Digit_String index type from Integer to Natural
    This simplifies the precondition on Is_Valid, and allows empty strings
    to be passed to the functions.

--- src/verhoeff.ads
+++ src/verhoeff.ads
@@ -24,15 +24,13 @@ package Verhoeff
 is
    subtype Digit_Character is Character range '0' .. '9';
    
-   type Digit_String is array(Integer range <>) of Digit_Character;
+   type Digit_String is array(Natural range <>) of Digit_Character;
    
    function Check_Digit(Seq : in Digit_String) return Digit_Character;
    -- Compute the verhoeff check digit on a sequence.
    
    function Is_Valid(Seq : in Digit_String) return Boolean
-      with Pre => (Seq'Length > 0 
-                   and
-                   Seq'Last > Integer'First);
+      with Pre => Seq'Length > 0;
    -- Validate the check digit on a sequence.
    --
    -- The check digit must be the last digit in the sequence.
